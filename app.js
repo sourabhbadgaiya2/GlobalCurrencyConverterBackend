893146a9ccb190642a40e24697c2aed8b13e3c0c
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-11-25 17:32:03 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const cors = require("cors");
 const app = express();
 
 //! cors
-app.use(cors({ origin: "http://localhost:5173/" }));
+const coreOptions = {
+  origin: ["http://localhost:5173/"],
+};
+app.use(cors(coreOptions));
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, //! 15 minutes
