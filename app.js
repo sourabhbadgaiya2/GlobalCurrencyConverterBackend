94fd546ba581ec29394514f7538f3061f6627079
sourabhbadgaiya2 <sourabhbadgaiya2@gmail.com>
2024-11-25 16:54:18 +0530

    final

---
+++ app.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+const axios = require("axios");
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+const app = express();
+
+//! cors
+app.use(cors({ origin: true }));
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //! 15 minutes
+  max: 100, //! Max 100 requests per 15 minutes
+  message: "Too many requests from this IP, please try again later.",
+});
+
+// !middleware
+app.use(express.json());
+app.use(apiLimiter);
+app.use(express.urlencoded({ extended: true }));
+
+//! Conversion
+const API_URL = "https://v6.exchangerate-api.com/v6/";
+const API_KEY = process.env.API_KEY;
+
+app.post("/api/convert", async (req, res) => {
+  try {
+    const { from, to, amount } = req.body;
+    const url = `${API_URL}/${API_KEY}/pair/${from}/${to}/${amount}`;
+    const response = await axios.get(url);
+    if (response.data && response.data.result === "success") {
+      res.json({
+        base: from,
+        target: to,
+        conversionRate: response.data.conversion_rate,
+        convertedAmount: response.data.conversion_result,
+      });
+    } else {
+      res.json({
+        message: "Error Converting currency",
+        details: response.data,
+      });
+    }
+  } catch (error) {
+    res.json({
+      message: "Error Converting currency",
+      details: error.message,
+    });
+  }
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
